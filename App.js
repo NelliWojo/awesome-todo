56501d60ea7c80676bacd7e9bef20ad7d3d596a2
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-03-12 16:42:03 +0100

    init :tada: setup

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Clicked")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>Now this looks like a job for me so everybody just follows me cos we need a little contreversy and it feels so empty without me lalalala lalalala lalalala lalalala</Text>
       <StatusBar style="auto" />
     </View>
   );
