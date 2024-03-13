a5ecd626806ce16308774f68b6ab525ca0cc48cc
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-03-13 10:12:06 +0100

    feat : bento: add task component & styles

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import TaskCard from "./components/TaskCard";
 
 export default function App() {
-  const handlePress = () => console.log("Clicked")
-
   return (
     <View style={styles.container}>
       {/* <Text numberOfLines={1} onPress={handlePress}>Now this looks like a job for me so everybody just follows me cos we need a little contreversy and it feels so empty without me lalalala lalalala lalalala lalalala</Text> */}
-      <Text>Test</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          <TaskCard text="Walk my dog" />
+          <TaskCard text="Go shopping" />
+          <TaskCard text="Finish my project" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F99FF9",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 20
   },
 });
